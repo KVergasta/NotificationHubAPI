b31c8724c9a06340cdbb8de8e567d0fe83f6b9ab
Kauane <kauane.rodrigues@emabsa.ba.gov.br>
2026-05-12 11:19:58 -0300

    remoção do cross do controller de teste

--- src/main/java/com/SpringNotificationHub/NotificationServ/controller/StreamController.java
+++ src/main/java/com/SpringNotificationHub/NotificationServ/controller/StreamController.java
@@ -3,7 +3,6 @@ package com.SpringNotificationHub.NotificationServ.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +12,7 @@ import com.SpringNotificationHub.NotificationServ.model.NotificationEntity;
 import com.SpringNotificationHub.NotificationServ.service.GeneratorNotification;
 import com.SpringNotificationHub.NotificationServ.service.StreamService;
 
-@CrossOrigin(origins = "http://localhost:4300")
+// controller para testar o kafka
 @RestController
 @RequestMapping("/stream")
 public class StreamController {
